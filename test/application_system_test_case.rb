50ccb52b2ea3e72dd32cfb84808a573867f4a593
Mitch Mele <mitchmele@gmail.com>
2018-01-06 10:33:34 -0600

    Clean up app system test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,22 +3,4 @@ require "test_helper"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
-  end
-
-  test "should get help" do
-    get static_pages_help_url
-    assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
-  end
-
-  test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-  end
-
 end
